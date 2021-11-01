c39aba1f55219095d126f3ea23faf045adb2c07d
diosleonor <tccoolovl@gmail.com>
2021-11-01 16:01:19 +0800

    add update function

--- app.js
+++ app.js
@@ -39,12 +39,12 @@ app.get('/', (req,res) => {
 	.catch(error => console.log(error))
 })
 
-// 設定New頁面路由
+// 設定new頁面路由
 app.get('/restaurants/new', (req, res) => {
 	return res.render('new')
 })
 
-// 取得New的表單資料並新增到資料庫
+// 取得new的表單資料並新增到資料庫
 app.post('/restaurants', (req, res) => {
 	const data = req.body
 	return Restaurant.create(data)
@@ -53,11 +53,43 @@ app.post('/restaurants', (req, res) => {
 })
 
 // 設定detail頁面路由:應用 params 打造動態路由
-app.get('/restaurants/:restaurants_id', (req, res) => {
-	const id = req.params.restaurants_id
+app.get('/restaurants/:restaurant_id', (req, res) => {
+	const id = req.params.restaurant_id
 	return Restaurant.findById(id)
 		.lean()
 		.then(restaurant => res.render('detail', { restaurant }))
+		.catch(error => console.log(error))
+})
+
+// 設定edit頁面路由:同detail頁面
+app.get('/restaurants/:restaurant_id/edit', (req, res) => {
+	const id = req.params.restaurant_id
+	return Restaurant.findById(id)
+		.lean()
+		.then(restaurant => res.render('edit', { restaurant }))
+		.catch(error => console.log(error))
+})
+
+// 取得edit的表單資料並修改到資料庫
+app.post('/restaurants/:restaurant_id/edit', (req, res) => {
+	const id = req.params.restaurant_id
+	const editedName = req.body.name
+	const editedNameEn = req.body.name_en
+	const editedCategory = req.body.category
+	const editedNameLocation = req.body.location
+	const editedNamePhone = req.body.phone
+	const editedNameDescription = req.body.description
+	return Restaurant.findById(id)
+		.then(restaurant => {
+			restaurant.name = editedName
+			restaurant.name_en = editedNameEn
+			restaurant.category = editedCategory
+			restaurant.location = editedNameLocation
+			restaurant.phone = editedNamePhone
+			restaurant.description = editedNameDescription
+			return restaurant.save()
+		})
+		.then(() => res.redirect(`/restaurants/${id}`))
 		.catch(error => console.log(error))
 })
 
