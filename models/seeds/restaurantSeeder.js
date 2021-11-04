749c32d3be7eb12c3e34686a97559ffa17cbc831
diosleonor <tccoolovl@gmail.com>
2021-11-04 17:53:00 +0800

    add search function, add default image and modify some codes

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,12 +1,10 @@
 // include restauranr model
 const Restaurant = require('../restaurant')
 // include restaurant.json
-const restaurants = require('../restaurant.json')
+const restaurants = require('../restaurant.json').results
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
-	for(let i = 0; i < 8; i++){
-		Restaurant.create(restaurants.results[i])
-	}
+	Restaurant.create(restaurants)
 	console.log('Done.')
 })
