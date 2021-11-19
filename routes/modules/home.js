58ac9faabf42ae5c11de60dfcda918cc88aed106
diosleonor <tccoolovl@gmail.com>
2021-11-19 22:17:15 +0800

    condense sort function codes

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -14,45 +14,16 @@ router.get('/', (req,res) => {
 //搜尋功能:
 router.get('/search',(req, res) => {
 	const keyword = req.query.keyword.toLowerCase().trim()
-	// 以下新增
- 	const sortItem = req.query.sort
-   	const sortMethod = {}
-  	let sortInput
-	   switch (sortItem) {
-	     case 'name_asc':
-	       sortMethod['name'] = 'asc'
-	       sortInput = `A > Z`
-	       break
-	     case 'name_desc':
-	       sortMethod['name'] = 'desc'
-	       sortInput = 'Z > A'
-	       break
-	     case 'rating_desc':
-	       sortMethod['rating'] = 'desc'
-	       sortInput = '評分最高'
-	       break
-	     case 'category':
-	       sortMethod['category'] = 'asc'
-	       sortInput = '類別'
-	       break
-	     case 'location':
-	       sortMethod['location'] = 'asc'
-	       sortInput = '地區'
-	       break
-	     default:
-	       sortMethod['_id'] = 'asc'
-	   }
-	// 新增結束
+ 	const [property, sortBy] = req.query.sort.split('_')
+ 	const sortInput = req.query.sort
 	Restaurant.find()
 		.lean()
-		// 新增
-		.sort(sortMethod)
-		// 新增結束
+		.sort({[property]: sortBy})
 		.then(restaurants => {
        		restaurants = restaurants.filter(restaurant => {
          		return restaurant.name.toLowerCase().includes(keyword) || restaurant.name_en.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
        	})
-       	res.render('index', { restaurants, keyword, sortInput })
+       	res.render('index', { restaurants, keyword, property, sortBy, sortInput})
      })
 		.catch(error => console.log(error))
 })
