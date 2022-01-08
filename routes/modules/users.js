9eb8c79b7407c7216c07206d397a731cf67acfe1
diosleonor <tccoolovl@gmail.com>
2022-01-08 17:10:08 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -10,10 +10,12 @@ router.get('/login', (req, res) => {
 })
 
 // 取得login的表單資料並登入
-router.post('/login', passport.authenticate('local', {
-	successRedirect:'/',
-	failureRedirect:'/users/login'
-}))
+router.post('/login', 
+	passport.authenticate('local', {
+		failureRedirect:'/users/login'
+	}),
+	(req, res) => {res.redirect('/')}
+	)
 
 // 設定register頁面路由
 router.get('/register', (req, res) => {
@@ -23,31 +25,41 @@ router.get('/register', (req, res) => {
 // 取得register的表單資料並新增到資料庫
 router.post('/register', (req, res) => {
 	const {name, email, password, confirmPassword} = req.body
-	User.findOne({email})
-		.then(user => {
-			if(user){
-				console.log('User already exists.')
-				res.render('register',{
-					name,
-					email,
-					password,
-					confirmPassword
-				})
-			} else {
-				return User.create({
-					name,
-					email,
-					password,
-				})
-				.then(() => res.redirect('/'))
-				.catch(err => console.log(err))
-			}
+	const errors = []
+	if(!name || !email || !password || !confirmPassword){
+		errors.push({message:'所有欄位都是必填。'})
+	}
+	if(password !== confirmPassword){
+		errors.push({message:'密碼與確認密碼不相符！'})
+	}
+	if(errors.length){
+		return res.render('register', {
+			errors, name, email, password, confirmPassword
 		})
+	}
+	User.findOne({email}).then(user => {
+		// 已註冊
+		if(user){
+			errors.push({message:'這個 Email 已經註冊過了。'})
+			return res.render('register',{
+				name,
+				email,
+				password,
+				confirmPassword
+			})
+		}
+		// 未註冊
+		return User.create({ name, email, password})
+			.then(() => res.redirect('/'))
+			.catch(err => console.log(err))
+	})
 })
+
 
 // 設定logout頁面路由
 router.get('/logout', (req, res) => {
 	req.logout()
+	req.flash('success_msg','你已成功登出。')
 	res.redirect('/users/login')
 })
 
