702177b9c6721e42d1f1c5de373ba98b151a9ef8
diosleonor <tccoolovl@gmail.com>
2022-01-08 19:09:38 +0800

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 module.exports = app =>{
 	// 初始化設定
 	app.use(passport.initialize())
@@ -19,13 +20,15 @@ module.exports = app =>{
 					false, 
 					req.flash('warning_msg','此帳號尚未註冊！')
 				)}
-			if(user.password !== password) 
-				{return done(
-					null,
-					false, 
-					req.flash('warning_msg', '帳號或密碼輸入錯誤！')
-				)}
-			return done(null, user)
+			return bcrypt.compare(password, user.password).then(isMatch => {
+				if(!isMatch) {
+					return done(
+						null,
+						false, 
+						req.flash('warning_msg', '帳號或密碼輸入錯誤！')
+					)}
+					return done(null, user)
+			})
 		})
 		.catch(err => done(err, false))
 	}))
